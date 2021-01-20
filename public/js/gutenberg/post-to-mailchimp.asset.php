5d152548f8a809e1d7bed974d8653efdff4465c8
EdwardBock <edward.bock@palasthotel.de>
2021-01-20 08:17:45 +0100

    transpiled

--- public/js/gutenberg/post-to-mailchimp.asset.php
+++ public/js/gutenberg/post-to-mailchimp.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-components', 'wp-data', 'wp-date', 'wp-edit-post', 'wp-element', 'wp-plugins', 'wp-polyfill'), 'version' => '1f48af52f09230f3ebb9dbb38f47cbc9');
+<?php return array('dependencies' => array('wp-api-fetch', 'wp-components', 'wp-data', 'wp-date', 'wp-edit-post', 'wp-element', 'wp-plugins', 'wp-polyfill'), 'version' => 'b66a27a941520a2ade8a6f8631e21183');
